c20cd37d0db9e727a2e2ac51de22866997a0050d
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2022-06-28 16:39:17 -0300

    new version

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.tadeuespindolapalermo.itprojectsupport.array"},{"l":"com.github.tadeuespindolapalermo.itprojectsupport.examples"},{"l":"com.github.tadeuespindolapalermo.itprojectsupport.utilities"}];updateSearchResults();
